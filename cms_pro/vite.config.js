35392b28e5abe3f74e0cc6f29e014cba93851417
Pradeep <2312034@nec.edu.in>
2025-10-30 14:44:58 +0530

    Add base path for Netlify deployment

--- cms_pro/vite.config.js
+++ cms_pro/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "./",
   plugins: [react()],
   preview: {
     allowedHosts: ['community-management-system-final-wjfi.onrender.com', 'community-management-system-final-9bku.onrender.com', 'community-management-system-final-klv9.onrender.com'],
