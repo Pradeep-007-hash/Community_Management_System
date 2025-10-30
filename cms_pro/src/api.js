0ea53f0d509bdd537283ac651ee04eecce1f45e8
Pradeep <2312034@nec.edu.in>
2025-10-30 11:56:13 +0530

    Update API_URL to use environment variable for deployment

--- cms_pro/src/api.js
+++ cms_pro/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000"; // backend server
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000"; // backend server
 
 // Signup
 export const signupUser = async (userData) => {
