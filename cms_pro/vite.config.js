fc02b6079d6e8c9214f4537484ec9e23f1409cc5
Pradeep <2312034@nec.edu.in>
2025-10-30 14:03:03 +0530

    Add new Render host to allowedHosts in vite.config.js

--- cms_pro/vite.config.js
+++ cms_pro/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   preview: {
-    allowedHosts: ['community-management-system-final-wjfi.onrender.com'],
+    allowedHosts: ['community-management-system-final-wjfi.onrender.com', 'community-management-system-final-9bku.onrender.com'],
     port: process.env.PORT || 4173
   }
 })
